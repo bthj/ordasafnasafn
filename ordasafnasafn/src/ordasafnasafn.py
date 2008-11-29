50e50bcea45efb94989c566327ba4b3ba2241d8d
bangsi@bthj.is <bangsi@bthj.is@30d6adc4-813a-0410-a748-6fec79a674fa>
2008-11-29 01:37:09 +0000

    initial implementation for ismal using genreal scraping. added general postSearch method to base class..

--- ordasafnasafn/src/ordasafnasafn.py
+++ ordasafnasafn/src/ordasafnasafn.py
@@ -7,9 +7,22 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import urlfetch
 
 class Search(webapp.RequestHandler):
+    def get(self):
+        self.searchstring = self.request.get('q')
     @classmethod
-    def getSearch(cls, search_url):
-        result = urlfetch.fetch(search_url)
+    def getSearch(cls, search_url, search_params):
+        result = urlfetch.fetch(search_url + "?" + urllib.urlencode(search_params))
+        if result.status_code == 200:
+            return result.content
+        else:
+            return "<h1>Ekki tókst að framkvæma leitina</h1>" #TODO: throw error
+    @classmethod
+    def postSearch(cls, search_url, search_params):
+        form_data = urllib.urlencode(search_params)
+        result = urlfetch.fetch(url=search_url, 
+                                payload=form_data, 
+                                method=urlfetch.POST, 
+                                headers={'Content-Type': 'application/x-www-form-urlencoded'})
         if result.status_code == 200:
             return result.content
         else:
@@ -30,10 +43,10 @@ class SearchHugtakasafn(Search):
     search_params = {"tungumal" : "oll"}
     filter_pattern = "<dl>(.*)</dl>"
     def get(self):
-        searchstring = self.request.get('q')
-        self.search_params["leitarord"] = searchstring
-        search_url = self.base_url + "leit-nidurstodur.adp?" + urllib.urlencode(self.search_params)
-        search_results = self.getSearch(search_url).decode('iso-8859-1')
+        Search.get(self)
+        self.search_params["leitarord"] = self.searchstring
+        search_url = self.base_url + "leit-nidurstodur.adp"
+        search_results = self.getSearch(search_url, self.search_params).decode('iso-8859-1')
         filtered_results = self.filterSearch(self.filter_pattern, search_results)[0]
         if len(filtered_results) > 1:
             target_results = self.addTargetToLinks(filtered_results)
@@ -41,11 +54,27 @@ class SearchHugtakasafn(Search):
         else:
             returned_results = "<h1>Ekkert fannst</h1>"
         self.response.out.write(returned_results)
+        
+class SearchIsmal(Search):
+    base_url = "http://www.ordabanki.ismal.hi.is/"
+    search_params = {
+        "searchlanguage" : "EN", 
+        "targetlanguage" : "", 
+        "Submit" : "Leita"
+    }
+    filter_pattern = "Listi yfir niðurstöður byrjar.*?<td.*?>(.*?</table>).*Listi yfir niðurstöður endar"
+    def get(self):
+        Search.get(self)
+        self.search_params["searchphrase"] = self.searchstring
+        search_url = self.base_url + "search"
+        search_results = self.postSearch(search_url, self.search_params).decode('iso-8859-1')
+        filtered_results = self.filterSearch(self.filter_pattern, search_results)[0]
+        self.response.out.write(filtered_results)
 
-
-application = webapp.WSGIApplication(
-                                     [('/hugtakasafn', SearchHugtakasafn)], 
-                                     debug=True)
+application = webapp.WSGIApplication([
+                                      ('/hugtakasafn', SearchHugtakasafn), 
+                                      ('/ismal', SearchIsmal)
+                                      ], debug=True)
 
 def main():
     run_wsgi_app(application)
